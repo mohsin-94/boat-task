722b95a4b5c42720ddb9672c4c526ce0f5f1a889
mohsin.khan3 <mohsin.khan@skytesters.com>
2022-01-10 00:14:39 +0530

    five  commit

--- src/components/innerHomeComp/JobPostCard.js
+++ src/components/innerHomeComp/JobPostCard.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 function JobPostCard({title,location,description}) {
     return (
-        <div className='col-sm-4'>
+        <div className='col-sm-4 mb-4'>
             <div className="card">
                 <div className="card-body">
                     <h5 className="card-title">{title}</h5>
